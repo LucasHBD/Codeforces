bbd5bdd6d573aafabc8799b135db55ae8754b0e2
LucasHBD <130990709+LucasHBD@users.noreply.github.com>
2023-07-12 15:33:49 -0300

    A.cpp

--- C++/Lista2/A.cpp
+++ C++/Lista2/A.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
 
 int main(){
     ios_base::sync_with_stdio(false);
-    int n;
+    int n, a;
     cin >> n;
-    vector<int> a;
-    for(int i=0; i<= n; ++i){
-        int x;
-        cin >> x;
-        a.push_back(x);
+    vector<int> v;
+    int qtda = 0;
+    int qtdb = 0;
+    double long media = 0;
+    for (int i=1; i<=n; ++i){
+        cin >> a;
+        v.push_back(a);
+        media += a;
     }
-    int soma = 0;
-    for(int i=0; i<a.size(); i++){
-        soma += a[i];
+    media = media/n;
+    for (int i = 0; i<n; i++){
+        if(v[i] < media){
+            qtdb += 1;
+        }
+        if(v[i] >= media){
+            qtda += 1;
+        }
     }
-    double media = soma/a.size();
-    cout << media << "\n";
+    cout << setprecision(1) << fixed << media << "\n" << qtdb << "\n" << qtda << "\n";
     return 0;
 }
