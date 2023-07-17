ecf5a32d728ffea3f8a596258ea01d8fad448ccf
LucasHBD <130990709+LucasHBD@users.noreply.github.com>
2023-07-17 13:07:58 -0300

    space_hell.py

--- PyGame/meu-jogo/v1/space_hell.py
+++ PyGame/meu-jogo/v1/space_hell.py
@@ -1,6 +1,5 @@
 import sys
 import time
-
 import pygame
 
 def game_init(w, h):
@@ -10,7 +9,7 @@ def game_init(w, h):
     pygame.display.set_caption("Space Hell")
     return display
 
-#Personagem
+# Personagem
 def draw_player(ship, ship_rect, display):
     ship_copy = ship.copy()
     display.blit(ship_copy, ship_rect)
@@ -19,9 +18,9 @@ def draw_boss(boss, boss_rect, display):
     boss_copy = boss.copy()
     display.blit(boss_copy, boss_rect)
 
-#Disparos
+# Disparos
 def create_shot(x, y):
-    shot = {'rect': pygame.Rect(x, y, 10, 10), 'color': (0,255,0,0)}
+    shot = {'rect': pygame.Rect(x, y, 10, 10), 'color': (0, 255, 0, 0)}
     shots.append(shot)
 
 def update_shots():
@@ -33,27 +32,27 @@ def draw_shots():
         pygame.draw.rect(display, shot['color'], shot['rect'])
 
 def create_boss_shot(x, y):
-    bshot = {'rect': pygame.Rect(x, y, 10, 10), 'color': (255,0,0,0)}
-    shots.append(bshot)
+    bshot = {'rect': pygame.Rect(x, y, 10, 10), 'color': (255, 0, 0, 0)}
+    boss_shots.append(bshot)
 
 def update_boss_shots():
     for bshot in boss_shots:
-        bshot['rect'].y -= boss_shot_speed
+        bshot['rect'].y += boss_shot_speed
 
 def draw_boss_shots():
     for bshot in boss_shots:
         pygame.draw.rect(display, bshot['color'], bshot['rect'])
 
-
-
-#Colisão
+# Colisão
 def collision_ship(ship_rect, boss_rect):
     if ship_rect.colliderect(boss_rect):
-        pygame.QUIT()
+        pygame.quit()
+        sys.exit()
 
 def collision_boss(boss_rect, ship_rect):
     if boss_rect.colliderect(ship_rect):
-        pygame.QUIT()
+        pygame.quit()
+        sys.exit()
 
 def collision_bullet():
     global current_health
@@ -68,18 +67,19 @@ def collision_boss_bullet():
         if bshot['rect'].colliderect(ship_rect):
             current_health2 -= 10
             boss_shots.remove(bshot)
-#Health Bar
+
+# Health Bar
 def health_bar():
-    health_w = (current_health/ max_health) * health_bar_w
+    health_w = (current_health / max_health) * health_bar_w
     health_bar_rect = pygame.Rect(health_bar_x, health_bar_y, health_w, health_bar_h)
     pygame.draw.rect(display, health_bar_color, health_bar_rect)
 
 def health_bar2():
-    health_w2 = (current_health2/ max_health2) * health_bar_w2
+    health_w2 = (current_health2 / max_health2) * health_bar_w2
     health_bar_rect2 = pygame.Rect(health_bar_x2, health_bar_y2, health_w2, health_bar_h2)
     pygame.draw.rect(display, health_bar_color2, health_bar_rect2)
 
-#Movimentação
+# Movimentação
 def move_player(keys, ship_rect):
     if keys[pygame.K_d]:
         ship_rect.x += 5
@@ -96,7 +96,7 @@ def move_player(keys, ship_rect):
         if ship_rect.top < 300:
             ship_rect.top = 300
     if keys[pygame.K_s]:
-        ship_rect.y +=5
+        ship_rect.y += 5
         if ship_rect.bottom > 600:
             ship_rect.bottom = 600
 
@@ -114,28 +114,28 @@ def move_boss(keys, boss_rect):
         if boss_rect.top < 0:
             boss_rect.top = 0
     if keys[pygame.K_DOWN]:
-        boss_rect.y +=5
+        boss_rect.y += 5
         if boss_rect.bottom > 600:
             boss_rect.bottom = 600
         if boss_rect.bottom > 300:
-            boss_rect.bottom = 300              
+            boss_rect.bottom = 300
 
-display = game_init(800,600)
+display = game_init(800, 600)
 
-#Background
+# Background
 background_img = pygame.image.load("Space.jpg")
-#Personagens
+# Personagens
 ship = pygame.image.load("Nave.bmp")
-ship.set_colorkey((255,0,255))
+ship.set_colorkey((255, 0, 255))
 ship_rect = ship.get_rect()
 boss = pygame.transform.flip(pygame.image.load("Boss.bmp"), False, True)
-boss.set_colorkey((255,0,255))
+boss.set_colorkey((255, 0, 255))
 boss_rect = boss.get_rect()
-ship_rect.y = 600-boss_rect.bottom
+ship_rect.y = 600 - boss_rect.bottom
 ship_rect.x = 400
 boss_rect.y = 0
 boss_rect.x = 400
-#Barra de Vida
+# Barra de Vida
 max_health = 100
 current_health = 100
 health_bar_w = 200
@@ -150,7 +150,7 @@ health_bar_h2 = 10
 health_bar_x2 = 600
 health_bar_y2 = 580
 health_bar_color2 = (0, 255, 0)
-#Disparos
+# Disparos
 shots = []
 shot_speed = 8
 boss_shots = []
@@ -173,14 +173,11 @@ while True:
     if keys[pygame.K_RCTRL]:
         current_time = pygame.time.get_ticks()
         if current_time - last_shot_time > shot_cooldown:
-            create_boss_shot(boss_rect.x, boss_rect.y)
-            last_shot_time = current_time         
+            create_boss_shot(boss_rect.x, boss_rect.y + boss_rect.height)
+            last_shot_time = current_time
 
-    # player_shoot(keys, ship_rect)
     move_player(keys, ship_rect)
     move_boss(keys, boss_rect)
-
-    
 
     display.blit(background_img, (0, 0))
     draw_player(ship, ship_rect, display)
@@ -188,7 +185,7 @@ while True:
     update_shots()
     draw_shots()
     update_boss_shots()
-    draw_boss_shots()    
+    draw_boss_shots()
     health_bar()
     health_bar2()
     collision_ship(ship_rect, boss_rect)
@@ -196,4 +193,4 @@ while True:
     collision_bullet()
     collision_boss_bullet()
     pygame.display.flip()
-    clock.tick(60)       
+    clock.tick(60)
