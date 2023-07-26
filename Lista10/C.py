e516a721f2c66ca100d8e223729395cdd02120f3
LucasHBD <130990709+LucasHBD@users.noreply.github.com>
2023-07-26 09:56:21 -0300

    C.py

--- Lista10/C.py
+++ Lista10/C.py
@@ -1,14 +1,20 @@
-def contar_rep(texto):
-    qtd = {}
+def contar_rep(sequencia):
+    maior_tamanho = 0
+    tamanho_atual = 0
+    letra_anterior = None
 
-    for letra in texto:
-        if letra.isalpha():
-            letra = letra.upper()
-            qtd[letra] = qtd.get(letra, 0) + 1
-    if not qtd:
-        return 0
-    max_rep = max(qtd.values())
-    return max_rep
+    for letra in sequencia:
+        if letra == letra_anterior:
+            tamanho_atual += 1
+        else:
+            if tamanho_atual > maior_tamanho:
+                maior_tamanho = tamanho_atual
+            tamanho_atual = 1
+        letra_anterior = letra
+    if tamanho_atual > maior_tamanho:
+        maior_tamanho = tamanho_atual
+    
+    return maior_tamanho
 
 n = input()
 print(contar_rep(n))
