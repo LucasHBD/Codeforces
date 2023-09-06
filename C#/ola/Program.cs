24a81517fd4b46ab9e256a6c50629ef2c4b99d9c
LucasHBD <130990709+LucasHBD@users.noreply.github.com>
2023-09-06 11:17:04 -0300

    Add files via upload

---
+++ C#/ola/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+class Program{
+    // public static void Main(){
+    //     Console.WriteLine("Digite seu nome completo: ");
+    //     string s = Console.ReadLine();
+
+    //     int i = s.IndexOf(' ');
+    //     Console.WriteLine($"Bem Vindo ao C#, {s.Substring(0, i)}");
+    // }
+    public static void Main(){
+        int km = 300000;
+        Console.WriteLine("Digite o intervalo de tempo em \"HH:MM:SS");
+        int[] x =
+        Console.ReadLine().Split(':').Select(int.Parse).ToArray();
+        long hora = x[0] * 3600;
+        long minuto = x[1] * 60;
+        long segundos = x[2];
+
+        long soma = hora + minuto + segundos;
+        long mult = km * soma;
+
+        Console.WriteLine("A luz percorreu " + mult + " km nesse intervalo");
+    }
+}
