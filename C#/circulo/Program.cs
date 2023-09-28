91f2d872224999d4d6da3fe793367cf61abb41c8
LucasHBD <130990709+LucasHBD@users.noreply.github.com>
2023-09-28 14:07:50 -0300

    Add files via upload

---
+++ C#/circulo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program{
+    public static void Main(){
+        Circulo a = new Circulo();
+        Circulo b = new Circulo();
+        Circulo c = new Circulo();
+        Console.WriteLine(a.CalcArea(2));
+        Console.WriteLine(a.Circunferencia(2));
+        Console.WriteLine(b.CalcArea(20));
+        Console.WriteLine(b.Circunferencia(20));
+        Console.WriteLine(c.CalcArea(35));
+        Console.WriteLine(c.Circunferencia(35));
+    }
+}
+
+class Circulo{
+    int raio;
+    const double pi = 3.1415;
+    public double CalcArea(int raio){
+        double area = pi * (raio * raio);
+        return area;
+    }
+    public double Circunferencia(int raio){
+        double circ = 2 * pi * raio;
+        return circ;
+    }
+}
