9145466f32037e3b80586c2b65e68fb090652e64
LucasHBD <lucashdemetrio@hotmail.com>
2023-11-01 14:37:54 -0300

    push_back, front, pop_front e insert_at

--- C++/cpp/array_list.cpp
+++ C++/cpp/array_list.cpp
@@ -5,21 +5,23 @@ using namespace std;
 class array_list {
 private:
     int* data;
-    unsigned int size_, capacity_;
+    unsigned int x, size_, capacity_;
     void increase_capacity() {
         if(size_ == capacity_){
-            unsigned int new_capacity = capacity_ *2;
-            int *new_data = new int[new_capacity]; 
-            delete[] data;
-            data = new_data;
-            capacity_ = new_capacity;
+            int *new_array = new int[capacity_ + capacity_];
+            for(int i = 0; i<size_; ++i){new_array[i] = data[i];}
+            int *old_array = data;
+            data = new_array;
+            delete[] old_array;
+            capacity_ = capacity_ + capacity_;
         }
+        data[size_];
     }
 public:
     array_list() {
-        data = new int[8];
+        data = new int[10];
         this->size_ = 0;
-        this->capacity_ = 8;
+        this->capacity_ = 10;
     }
     ~array_list() {
         delete[] data;
@@ -33,7 +35,9 @@ public:
     double percent_occupied() {}
     bool insert_at(unsigned int index, int value) {
         if(index >= size_)return false;
-        for(int i = index + 1; i<this ->size_; i++) this ->data[i-1] = value;
+        if(size_ == capacity_) increase_capacity();
+        for(int i = size_; i> index; i--) this ->data[i] = this ->data[i -1];
+        this ->data[index] = value;
         this ->size_++;
         return true;
     }
@@ -61,8 +65,19 @@ public:
         this ->data[size_--] = value;
     }
     bool pop_back() {}
-    bool pop_front() {}
-    int front(){}
+    bool pop_front() {
+        if(size_ == 0) return false;
+        int *new_data = new int[size_ -1];
+        for(int i = 1; i<size_; ++i){new_data[i-1] = data[i];}
+        delete[] data;
+        data = new_data;
+        size_--;
+        return true; 
+    }
+    int front(){
+        if(size_ == 0) return -1;
+        return data[0];
+    }
     int back(){}
     bool remove(int value) {}
     int find(int value) {}
@@ -73,8 +88,14 @@ public:
 int main(){
     array_list *lista1 = new array_list;
     for(int i = 1; i<=10; i++){
-        lista1 ->push_front(i);
+        lista1 ->push_back(i);
     }
+    lista1 ->push_back(11);
+    lista1 ->push_back(12);
+    lista1 ->push_back(13);
+    lista1 ->push_back(14);
+    lista1 ->insert_at(10, 45);
+    lista1 ->push_back(15);
     // cout << "Sum = " << lista1 ->sum() << endl;
     // lista1 ->insert_at(2, 23);
     // lista1 ->insert_at(0, 30);
@@ -87,9 +108,9 @@ int main(){
     // cout << "O menor valor da lista: " << lista1 ->min() << endl;
     // cout << "O Tamanho da Lista: " << lista1 ->size() << endl;
     cout << "lista1 = {";
-    for(int i = 0; i<11; i++){
-        cout << lista1 ->front() << " ";
-        //lista1 ->pop_front();
+    for(int i = 0; i<16; i++){
+        cout << lista1-> front()<< " ";
+        lista1 ->pop_front();
     }
     cout << "}" << endl;
     delete lista1;
