d8cdd3c53e0e9f39724e87babbcf3ea9c7cd9246
LUCAS HENRIQUE BARRETO DEMETRIO <20231014040004@ifrn.local>
2024-03-19 08:06:52 -0300

    questão 1 lista algoritmos placido

--- C++/Lista2/q1.cpp
+++ C++/Lista2/q1.cpp
@@ -13,7 +13,20 @@ int main(){
             reset++;
             soma += MAT[i][j];
             if(reset == 8){
-                cout << "soma da linha " << i + 1 << " = " << soma;
+                cout << "soma da linha " << i + 1 << " = " << soma << endl;
+                if(soma % 2 == 0){ cout << " par" << endl;} else {cout << " impar" << endl;}
+                reset = 0;
+                soma = 0;
+            }
+        }
+    }
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 8; j++){
+            reset++;
+            soma += MAT[i][j];
+            if(reset == 4){
+                cout << "soma da coluna " << j + 1 << " = " << soma;
+                if(soma % 2 == 0){ cout << " par" << endl;} else {cout << " impar" << endl;}
                 reset = 0;
                 soma = 0;
             }
